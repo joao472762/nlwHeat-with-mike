4983e4cd173cfd18b6017b10cb011b7083b2530c
João Victor de Souza Oliveira <tygasouzaa@gmail.com>
2022-01-18 17:21:07 -0300

    mudei o link  pelo javaScript

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,6 +1,22 @@
-function showUserName(userName='afonso'){
-    alert(userName)
+function showUserName(userName={}){
+    window.alert(userName.twitter)
 }
-let SocialMidea = {
-    facebook
+let socialMidea = {
+    facebook:"rocketseat",
+    instagran:"progama_ai",
+    youtube: "planetanovo",
+    twitter: "cursosemvideo"
 }
+function ChangeUserName(newName='root'){
+    userName.textContent = newName
+}
+
+
+function changeSocialMediaLinks(){
+    for( li of socialLinks.children){
+        const social =  li.getAttribute('class')
+
+        li.children[0].href = `https://${social}.com/${socialMidea[social]}`
+    }
+}
+changeSocialMediaLinks()
