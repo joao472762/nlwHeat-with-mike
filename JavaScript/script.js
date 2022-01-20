4f852d400483ae569f39f864f5ed39c1755cafcb
João Victor de Souza Oliveira <tygasouzaa@gmail.com>
2022-01-20 03:53:21 -0300

    finish

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,6 +1,8 @@
+
 let handeleChangeUserLinks = {
 
     userMidea: {
+        github: 'joao472762',
         youtube: 'travisscott',
         twitter: 'cactusJack',
         facebook: 'traviscott',
@@ -22,3 +24,23 @@ let handeleChangeUserLinks = {
     }
 }
 handeleChangeUserLinks.execute()
+
+function changeUserDatas(name , bio, avatar, githubLink){
+    userName.textContent = name
+    userBio.textContent = bio
+    userAvatar.src = avatar
+    userGitHubLink.href = githubLink
+    userGitHubLink.children[1].textContent = handeleChangeUserLinks.userMidea.github
+}
+
+
+function getGitHubProfileInfos(){
+    const url = `https://api.github.com/users/${handeleChangeUserLinks.userMidea.github}`
+
+    fetch(url)
+    .then(response  => response.json())
+    .then(data => {
+        changeUserDatas(data.name, data.bio, data.avatar_url,data.html_url)
+    })
+}
+getGitHubProfileInfos()
