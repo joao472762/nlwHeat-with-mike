9908e6edd4dc8679c45846c9f20b85434865120e
João Victor de Souza Oliveira <tygasouzaa@gmail.com>
2022-01-21 21:47:37 -0300

    I refactored the code

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,22 +1,49 @@
-function showUserName(userName={}){
-    window.alert(userName.twitter)
-}
-let socialMidea = {
-    facebook:"rocketseat",
-    instagran:"progama_ai",
-    youtube: "planetanovo",
-    twitter: "cursosemvideo"
-}
-function ChangeUserName(newName='root'){
-    userName.textContent = newName
-}
+let handleChangeUserLinks = {
 
+    formatLink(socialNetwork = String,user = {}){
+        return (`https://${socialNetwork}.com/${user[socialNetwork]}`)
+    },
+    changeLinks(userMidea = {}){
+        for(li of socialLinks.children){
+            const socialNetwork = li.getAttribute('class')
+            li.children[0].href = this.formatLink(socialNetwork,userMidea)
+          
+        }
+    }
 
-function changeSocialMediaLinks(){
-    for( li of socialLinks.children){
-        const social =  li.getAttribute('class')
+}
+let changeUserGitHubProfileInfos = {
+    changeUserDatas(data){
+        userName.textContent = data.name
+        userBio.textContent = data.bio
+        userGithub.children[1].textContent = data.login
+        userAvatar.src = data.avatar_url
+    },
+    callGitHubApi(user){
+        const url = `https://api.github.com/users/${user}`
 
-        li.children[0].href = `https://${social}.com/${socialMidea[social]}`
+        const allUserDatas = fetch(url)
+        .then(response => response.json())
+        .then(data => this.changeUserDatas(data))
+        
+    },
+    
+}
+let app = {
+    init(){
+        const userMidea = {
+            github: 'joao472762',
+            youtube: 'biilieelish',
+            facebook: 'marvel',
+            instagram: 'milesmorales',
+            twitter: 'travisscott'
+        }
+        handleChangeUserLinks.changeLinks(userMidea)
+        changeUserGitHubProfileInfos.callGitHubApi(userMidea.github)
     }
 }
-changeSocialMediaLinks()
+app.init()
+
+
+
+
